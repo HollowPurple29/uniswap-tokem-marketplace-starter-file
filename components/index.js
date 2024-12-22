654d06e20c1da003e744aa26cdd1350007da4d3c
SACHIN PRASAD <sachinprasad749@gmail.com>
2024-12-22 15:55:39 +0530

    initial commit

---
+++ components/index.js
@@ -0,0 +1,37 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Card from "./Card";
+import HeroSection from "./HeroSection";
+import Selector from "./Selector";
+import SingleCard from "./SingleCard";
+import SwapComponent from "./SwapComponent";
+import SwapField from "./SwapField";
+import Table from "./SwapField";
+import TokenBalance from "./TokenBalance";
+import TransactionStatus from "./TransactionStatus";
+
+//SVG ICON
+import Menu from "./SVG/Menu";
+import Logo from "./SVG/Logo";
+import Facebook from "./SVG/Facebook";
+import Insta from "./SVG/Insta";
+import Twitter from "./SVG/Twitter";
+
+export { 
+    Header,
+    Footer,
+    Card,
+    HeroSection,
+    Selector,
+    SingleCard,
+    SwapComponent,
+    Table,
+    TokenBalance,
+    TransactionStatus,
+    //SVG
+    Menu,
+    Logo,
+    Insta,
+    Facebook,
+    Twitter,
+};
